a54c03f1b4b56205e465a1ed49611e98b979f244
Yasin Erdemli <yasinerdemli35@icloud.com>
2023-03-01 12:36:52 +0300

    create app

---
+++ UberSwift/UberSwift/UberSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberSwiftApp.swift
+//  UberSwift
+//
+//  Created by Yasin Erdemli on 1.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct UberSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
