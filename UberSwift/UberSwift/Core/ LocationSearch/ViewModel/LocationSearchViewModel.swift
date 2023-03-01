2bbbc151321ac1891b98bc5ec5581be17a5eb64d
Yasin Erdemli <yasinerdemli35@icloud.com>
2023-03-01 20:39:50 +0300

    create localSearchViewModel and populate

---
+++ UberSwift/UberSwift/Core/ LocationSearch/ViewModel/LocationSearchViewModel.swift
@@ -0,0 +1,27 @@
+//
+//  LocationSearchViewModel.swift
+//  UberSwift
+//
+//  Created by Yasin Erdemli on 1.03.2023.
+//
+
+import Foundation
+import MapKit
+
+class LocationSearchViewModel: NSObject, ObservableObject {
+    @Published var results = [MKLocalSearchCompletion]()
+    private let searchCompleter = MKLocalSearchCompleter()
+    var queryFragment: String = ""
+    
+    override init() {
+        super.init()
+        searchCompleter.delegate = self
+        searchCompleter.queryFragment = queryFragment
+    }
+}
+
+extension LocationSearchViewModel: MKLocalSearchCompleterDelegate {
+    func completerDidUpdateResults(_ completer: MKLocalSearchCompleter) {
+        self.results = completer.results
+    }
+}
